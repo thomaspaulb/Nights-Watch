801316ead5b94a7de5110296d2be79ca98b2349a
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-03-19 09:09:02 +0300

    add model fields

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profiles/', null=True)
+    user_bio = models.TextField()
+    user = models.ForeignKey(User)
+    neighborhood = models.ForeignKey(Hood, null=True) 
+
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+class Hood(models.Model):
+    image = models.ImageField(upload_to = 'images/', null = True)
+    name = models.CharField(max_length=30)
+    status = models.TextField()
+    occupants = models.IntegerField(default=0)
+
+class Business(models.Model):
+    name = models.CharField(max_length=30)
+    profile = models.ForeignKey(Profile, null=True) 
+    neighborhood = models.ForeignKey(Hood, null=True) 
--- watch/settings.py
+++ watch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'swofp09zthy@pgacwykyl4hymce^e1u1#khj9amwo9jtlt@w_#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'watch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'seth',
+        'PASSWORD':'seth'
     }
 }
 
