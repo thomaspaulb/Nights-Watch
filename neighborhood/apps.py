cb1c94331b283d36155d4d39009f9ac0a01bbb88
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-03-21 15:21:03 +0300

    update post app

--- neighborhood/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NeighborhoodConfig(AppConfig):
-    name = 'neighborhood'
--- neighborhood/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,redirect
-from django.http  import HttpResponse,Http404,HttpResponseRedirect
-from django.contrib.auth.models import User
-from .models import Hood, Profile, Business
-
-# Create your views here.
-def index(request):
- 
-    return render(request, 'index.html')    
